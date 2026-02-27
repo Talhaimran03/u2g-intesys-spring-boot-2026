d0da7d12e2239c8eadb50b7d755c141fb7df5a28
talha.imran <talha.imran@intesys.it>
2026-02-27 15:58:11 +0100

    Lesson 5: implement card endpoints

---
+++ src/main/java/org/u2g/codylab/teamboard/entity/Card.java
@@ -0,0 +1,66 @@
+package org.u2g.codylab.teamboard.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cards")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @jakarta.persistence.Column(nullable = false)
+    private String title;
+
+    @jakarta.persistence.Column
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "assigned_to", nullable = false)
+    private User assignedTo;
+
+    @ManyToOne
+    @JoinColumn(name = "column_id", nullable = false)
+    private Column column;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(User assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
+    public void setColumn(Column column) {
+        this.column = column;
+    }
+}
